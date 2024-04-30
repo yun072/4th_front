d7b7ef7c882a5880c8dffa98a07f5a63435ecae4
hana <jjina.js@gmail.com>
2024-04-30 17:18:54 +0900

    백엔드 연결 설정 완료, 프론트 포트 -> 3000, 백엔드 포트 -> 8080

---
+++ src/services/axios.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// Axios 인스턴스 생성
+const axiosInstance = axios.create({
+    //baseURL: 'http://localhost:8080', // API의 기본 URL 설정
+});
+
+export default axiosInstance;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
+  devServer: {
+    proxy: {
+      '/api': { // 프록시할 경로 설정
+        target: 'http://localhost:8080', // 백엔드 서버의 주소 및 포트
+        changeOrigin: true
+      }
+    },
+    port: 3000 // 프론트엔드 개발 서버의 포트 설정
+  },
   transpileDependencies: true
-})
+});
